78520b7228fa9df4bc0580d516b9c110cf8f8289
Fuat Anshori <anshorifuat12@gmail.com>
2026-08-16 20:51:34 +0700

    chore: initial Django project setup

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+# core/urls.py
+
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.static import serve
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("dashboard.urls")),
+    path("", include("equipment.urls")),
+    path("", include("maintenance.urls")),
+    path("", include("reports.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
